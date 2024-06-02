8c11702f28effb4c7af36a6c759a75c6781dceaa
Mich-Tapawan <mik.tapawan@gmail.com>
2024-06-02 16:14:29 +0800

    Added loop for determining user action at the start of the game

--- slot-machine.py
+++ slot-machine.py
@@ -7,3 +7,26 @@ Objectives
 4. For every win, calculate the multiplier
 5. Update balance
 '''
+
+def deposit():
+    pass
+
+def place_bet():
+    pass
+
+print('||| SLOT MACHINE GAME |||\n')
+balance = 0
+while(True):
+    print(f'Current Balance: {balance}\n1. Deposit money\n2. Place a bet\n3.Exit')
+    decision = input("Enter the number of the option you want to choose: ")
+
+    if decision == '1':
+        deposit()
+    elif decision == '2':
+        place_bet()
+    elif decision == '3':
+        print('\nThank you for playing!')
+        break
+    else:
+        print('Invalid input! Try again.\n')
+        continue
