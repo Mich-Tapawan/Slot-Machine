905e28acc01ad6f7ff6ec087901447032965ccd0
Mich-Tapawan <mik.tapawan@gmail.com>
2024-06-02 15:57:48 +0800

    Added comments for marking the objectives

---
+++ slot-machine.py
@@ -0,0 +1,9 @@
+'''
+Objectives
+1. Deposit money to balance
+2. Select number of lines going to bet
+2. Place bet for each lines
+3. Spin the slot machine
+4. For every win, calculate the multiplier
+5. Update balance
+'''
